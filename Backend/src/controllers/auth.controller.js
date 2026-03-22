b131a1347fd9b74407a2273589596f4f40e80c2a
Vishuddhanand <vishupillewan2006@gmail.com>
2026-03-22 18:10:28 +0530

    Add user registration functionality with JWT authentication

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import userModel from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 async function registerUser(req, res) {
 
@@ -11,11 +13,33 @@ async function registerUser(req, res) {
         ]
     })
 
-    if(isUserAlreadyExists){
+    if (isUserAlreadyExists) {
         return res.status(400).json({
-            message:"User with the same email or username already exists"
+            message: "User with the same email or username already exists"
         })
     }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = userModel.create({
+        username,
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" })
+
+    res.cookie("token", token)
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        },
+        token
+    })
 
 
 }
